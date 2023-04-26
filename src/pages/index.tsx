9da88a4aabc9a7bdd8b3e127687b0e3e77ef7b06
Andre Derjagin <derjagin.andre@gmail.com>
2023-04-26 17:25:26 +0200

    added items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import awesomeIcon from '@/assets/svgs/awesome.svg';
+import awfulIcon from '@/assets/svgs/awful.svg';
+import badIcon from '@/assets/svgs/bad.svg';
+import goodIcon from '@/assets/svgs/good.svg';
+import neutralIcon from '@/assets/svgs/neutral.svg';
 import Layout from '@/layouts/Layout/Layout';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
@@ -6,6 +12,26 @@ export default function Home() {
 			<h1 className="text-4xl font-semibold text-center">Welcome back!</h1>
 			<section className="mt-32">
 				<h2 className="text-2xl font-semibold">How are you feeling?</h2>
+				<Image
+					src={awesomeIcon}
+					alt="Awesome Icon"
+				/>
+				<Image
+					src={goodIcon}
+					alt="Awesome Icon"
+				/>
+				<Image
+					src={neutralIcon}
+					alt="Awesome Icon"
+				/>
+				<Image
+					src={badIcon}
+					alt="Awesome Icon"
+				/>
+				<Image
+					src={awfulIcon}
+					alt="Awesome Icon"
+				/>
 			</section>
 		</Layout>
 	);
